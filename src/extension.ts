ab76f452843dbabe26d8abd83a2743ffdf306bdd
robin <robin@ansvia.com>
2019-08-30 00:56:30 +0700

    Begin implement syncgen code autogenerator based on edited sources.

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,19 @@
 
-import { ExtensionContext } from 'vscode';
+import { ExtensionContext, workspace, TextDocument } from 'vscode';
 
 import * as flutter from './flutter';
 import * as service from './server';
 import * as web from './web';
+import { setupSyncGen } from './syncgen';
 
-export function activate(context: ExtensionContext) {
+export function activate(context: ExtensionContext){
     flutter.setup(context);
 	service.setup(context);
 	web.setup(context);
+
+	setupSyncGen();
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
+
